6665cb6ca2f46792d04a3848a42ad05368a5cffb
Eric Warnke <ericew@gmail.com>
2020-06-02 09:04:35 -0400

    settings

--- inform_sniffer.py
+++ inform_sniffer.py
@@ -15,18 +15,14 @@ import requests
 
 app = Flask(__name__)
 
-real_url = 'http://192.168.111.20:8080/inform'
-
-
-
-#foo = packet_decode(a2b_hex("ba86f2bbe107c7c57eb5f2690775c712"), data)
+from settings import real_url, device_key
 
 @app.route("/inform", methods=['POST'])
 def inform():
     global dummy
     data = request.get_data()
 
-    inform = Packet(from_packet=data, key='1C6592EF70601F95D2B24A3A0B420B62')
+    inform = Packet(from_packet=data, key=device_key)
     print("FROM DEVICE: ", inform)
     #print(inform.payload_decoded)
     foo = json.loads(inform.payload_decoded)
@@ -36,16 +32,12 @@ def inform():
     raw_reply = out.content
     #print(type(raw_reply))
 
-    reply = Packet(from_packet=raw_reply, key='1C6592EF70601F95D2B24A3A0B420B62')
+    reply = Packet(from_packet=raw_reply, key=device_key)
 
     print("REPLY: ", reply.payload_decoded)
 
     return raw_reply
 
-
-
 #mca-ctrl -t connect -s "http://10.0.8.2:8080/inform" -k "A09E428C482EB53C7731C224295CD9D3"
-
-
 
 app.run(debug=True, port=18080, host='0.0.0.0')
